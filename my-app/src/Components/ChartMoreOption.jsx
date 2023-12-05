823f98f4c28dce82615a20bed8e4006b04308745
Parsa <parsa.habibi.2002@gmail.com>
2023-12-05 19:15:07 +0330

    refactor: fix animation for Open and close ChartMoreOption for Mobile and Desktop and Complete HomePage

--- my-app/src/Components/ChartMoreOption.jsx
+++ my-app/src/Components/ChartMoreOption.jsx
@@ -1,14 +1,14 @@
 import React, {useEffect, useRef, useState} from 'react'
-import {Grid, Typography, useMediaQuery, useTheme, styled, keyframes} from "@mui/material";
+import {Grid, Typography, useMediaQuery, useTheme} from "@mui/material";
 import {Link} from "react-router-dom";
 import {MoreHoriz} from "./Icons";
+import {useSpring , animated} from "@react-spring/web";
 
 const ChartMoreOption = () => {
 
 
     const [option, setOption] = useState(false)
 
-    const [closing, setClosing] = useState(false);
 
     const theme = useTheme()
 
@@ -18,30 +18,8 @@ const ChartMoreOption = () => {
 
     let variant = 'subtitle1';
 
-
-    const fadeIn = keyframes`
-      from {
-        opacity: 0;
-      }
-      to {
-        opacity: 1;
-      }
-    `;
-
-    const fadeOut = keyframes`
-      from {
-        opacity: 1;
-      }
-      to {
-        opacity: 0;
-      }
-    `;
-
-
-    const AnimatedGrid = styled(Grid)(({isClosing}) => ({
-        animation: isClosing ? `${fadeOut} 0.25s forwards` : `${fadeIn} 0.4s`,
-    }));
-
+    const padding = isXs ? '20px 20px 20px 100px' : '40px 40px 34px 166px';
+    const positionTop = isXs ? '28px' : '37px';
 
     if (isXs) {
         variant = 'subtitle1';
@@ -59,6 +37,11 @@ const ChartMoreOption = () => {
     const containerRef = useRef(null);
 
     const menuRef = useRef(null);
+
+
+    const optionAnimation = useSpring({
+        opacity: option ? 1 : 0,
+    });
 
     const handleClickOutside = (event) => {
         if (containerRef.current && !containerRef.current.contains(event.target)) {
@@ -82,38 +65,41 @@ const ChartMoreOption = () => {
               ref={containerRef}
         >
             <span onClick={() => {
-                setClosing(false) ;
                 setOption(!option)
             }} style={{paddingTop: '5px'}}>
                  <MoreHoriz fill={theme.palette.main}/>
             </span>
 
-            {
-                option ?
-                    <AnimatedGrid className={'itemsOption'}
-                                  p={{xs: '20px 20px 20px 100px', md: '40px 40px 34px 166px'}}
-                                  position={'absolute'} top={{xs: '28px', md: '37px'}} left={0} zIndex={10}
-                                  bgcolor={'white'} borderRadius={'16px'} border={'1px solid #B0BBD5'}
-                                  boxShadow={5}
-                                  ref={menuRef}
-                                  isClosing={closing}
-                    >
-                        {
-                            itemsOptionData.map(
-                                item =>
-                                    <Link to={item.link}>
-                                        <Typography whiteSpace={'nowrap'} variant={variant}
-                                                    pb={item.id === 3 ? '0' : '16px'}
-                                                    fontWeight={500}>{item.title}</Typography>
-                                    </Link>
-                            )
-                        }
-                    </AnimatedGrid>
-                    :
-                    <></>
-            }
-
-
+            <animated.div
+                className={'itemsOption'}
+                style={{
+                    opacity: optionAnimation.opacity,
+                    transformOrigin: 'top center',
+                    backgroundColor: 'white',
+                    borderRadius: '16px',
+                    border: '1px solid #B0BBD5',
+                    boxShadow: '5px 5px 10px rgba(0, 0, 0, 0.1)',
+                    position: 'absolute',
+                    top: positionTop,
+                    left: 0,
+                    padding,
+                    zIndex: 20,
+                }}
+                ref={menuRef}
+            >
+                {itemsOptionData.map((item) => (
+                    <Link key={item.id} to={item.link}>
+                        <Typography
+                            whiteSpace={'nowrap'}
+                            variant={variant}
+                            pb={item.id === 3 ? '0' : '16px'}
+                            fontWeight={500}
+                        >
+                            {item.title}
+                        </Typography>
+                    </Link>
+                ))}
+            </animated.div>
         </Grid>
 
 
